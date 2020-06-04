870aae62fccb679c7ed604754095d95c278ea326
hxt <lancy@hxtdeAir.fios-router.home>
2020-06-04 15:41:20 -0400

    update

--- project/content.js
+++ project/content.js
@@ -1,22 +1,22 @@
-var myImage = document.getElementById("mainImage");
+var my_image = document.getElementById("mainImage");
 
-var imageArray = ["images/G20.jpg","images/education.jpg", "images/health.jpg", "images/military.jpg"];
+var image_array = ["images/G20.jpg","images/education.jpg", "images/health.jpg", "images/military.jpg"];
 
-var imageIndex = 0;
+var image_index = 0;
 
-function changeImage() {
-    myImage.setAttribute("src", imageArray[imageIndex]);
-    imageIndex++;
-    if (imageIndex >= imageArray.length) {
-        imageIndex = 0;
+function change_image() {
+    my_image.setAttribute("src", image_array[image_index]);
+    image_index++;
+    if (image_index >= image_array.length) {
+        image_index = 0;
 
     }
 }
 
-var intervalHandle = setInterval(changeImage, 2180);
+var interval_handle = setInterval(change_image, 2180);
 
-myImage.onclick = function() {
+my_image.onclick = function() {
 
-    clearInterval(intervalHandle);
+    clearInterval(interval_handle);
 }
 
